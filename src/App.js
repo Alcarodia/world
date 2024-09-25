e3d7039ec1e3b167235f4da7d61918476ce91557
Caferino <caferinograal@hotmail.com>
2024-09-24 23:56:12 -0600

    Fixing routing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AuthorPage from './pages/content/AuthorPage';
 import ContactPage from './pages/content/ContactPage';
 import ProjectsPage from './pages/content/ProjectsPage';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
   /*** Methods ***/
@@ -23,46 +23,44 @@ function App() {
   return (
     <div className="Background" style={{ backgroundColor: color }}>
       <div className="Page">
-        <Router>
-          {/* Navbar */}
-          <div className="Sidebar">
-            <div className="nav-scrollable">
-              <nav className="vcenter-row">
-                <div className="nav-item">
-                  <NavLink activeclassname="nav-link.active" className="nav-link" to="/about">
-                    <span className="nav-title"> ABOUT </span></NavLink>
-                </div>
-                <div className="nav-item">
-                  <NavLink activeclassname="nav-link.active" className="nav-link" to="/projects">
-                    <span className="nav-title"> PROJECTS </span></NavLink>
-                </div>
-                <div className="nav-item">
-                  <NavLink activeclassname="nav-link.active" className="nav-link" to="/world">
-                      <span className="nav-title nav-main"> ALCARODIA </span></NavLink>
-                </div>
-                <div className="nav-item">
-                  <NavLink activeclassname="nav-link.active" className="nav-link" to="/author">
-                    <span className="nav-title"> AUTHOR </span></NavLink>
-                </div>
-                <div className="nav-item">
-                  <NavLink activeclassname="nav-link.active" className="nav-link" to="/contact">
-                    <span className="nav-title"> CONTACT </span></NavLink>
-                </div>
-              </nav>
-            </div>
+        {/* Navbar */}
+        <div className="Sidebar">
+          <div className="nav-scrollable">
+            <nav className="vcenter-row">
+              <div className="nav-item">
+                <NavLink activeclassname="nav-link.active" className="nav-link" to="/about">
+                  <span className="nav-title"> ABOUT </span></NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink activeclassname="nav-link.active" className="nav-link" to="projects">
+                  <span className="nav-title"> PROJECTS </span></NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink activeclassname="nav-link.active" className="nav-link" to="/">
+                    <span className="nav-title nav-main"> ALCARODIA </span></NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink activeclassname="nav-link.active" className="nav-link" to="/author">
+                  <span className="nav-title"> AUTHOR </span></NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink activeclassname="nav-link.active" className="nav-link" to="/contact">
+                  <span className="nav-title"> CONTACT </span></NavLink>
+              </div>
+            </nav>
           </div>
-            
-          {/* Content */}
-          <main className="Content">
-            <Routes>
-              <Route path="/about"    element={<HandleNavigation color={'#77DD77'} Path={AboutPage}    />} />
-              <Route path="/projects" element={<HandleNavigation color={'#E8CE72'} Path={ProjectsPage} />} />
-              <Route path="/world"    element={<HandleNavigation color={'#001F3F'} Path={HomePage}     />} />
-              <Route path="/author"   element={<HandleNavigation color={'#FD8787'} Path={AuthorPage}   />} />
-              <Route path="/contact"  element={<HandleNavigation color={'#424242'} Path={ContactPage}  />} />
-            </Routes>
-          </main>
-        </Router>
+        </div>
+          
+        {/* Content */}
+        <main className="Content">
+          <Routes>
+            <Route path="/about"    element={<HandleNavigation color={'#77DD77'} Path={AboutPage}    />} />
+            <Route path="/projects" element={<HandleNavigation color={'#E8CE72'} Path={ProjectsPage} />} />
+            <Route path="/"    element={<HandleNavigation color={'#001F3F'} Path={HomePage}     />} />
+            <Route path="/author"   element={<HandleNavigation color={'#FD8787'} Path={AuthorPage}   />} />
+            <Route path="/contact"  element={<HandleNavigation color={'#424242'} Path={ContactPage}  />} />
+          </Routes>
+        </main>
       </div>
     </div>
   );
